7b594fec177b33e6f609657e6adc3a046f10cf71
Vishnu <vishnuhnair1996@gmail.com>
2016-04-08 16:57:24 +0530

    Almost done

--- Part-A/ChangingProperties.java
+++ Part-A/ChangingProperties.java
@@ -3,43 +3,79 @@
 // import javax.swing.JMenuItem;
 // import javax.swing.JPopupMenu;
 // import java.awt.event.MouseEvent;
-// import java.awt.event.MouseAdapter;
+import java.awt.event.*;
 // import java.awt.event.EventListener;
 import java.awt.*;
 import javax.swing.*;
 
-public class ChangingProperties extends JFrame{
+public class ChangingProperties {
 
-  MouseListener ml = new MouseListener() {
-        @Override
-        public void mouseReleased(MouseEvent e) {System.out.println("Released!");}
+  private JFrame frame;
+  private JLabel label;
+  private JPanel panel;
 
-        @Override
-        public void mousePressed(MouseEvent e) {System.out.println("Pressed!");}
+  class showList {
 
-        @Override
-        public void mouseExited(MouseEvent e) {System.out.println("Exited!");}
+    private JMenuItem First;
+    private JMenuItem Properties;
+    private JMenuItem Third;
 
-        @Override
-        public void mouseEntered(MouseEvent e) {System.out.println("Entered!");}
+    public showList() {
 
-        @Override
-        public void mouseClicked(MouseEvent e) {System.out.println("Clicked!");}
-    };
+      First = new JMenuItem("First");
+      Properties = new JMenuItem("Properties");
+      Third = new JMenuItem("Third");
 
-  JLabel label = new JLabel("Changing Properties", SwingConstants.CENTER);
-  public static void main(String[] args){
-    new ChangingProperties();
+      Properties.addActionListener(new ActionListener() {
+
+        public void actionPerformed(ActionEvent e) {
+          propertiesClick();
+        }
+      });
+    }
+  }
+
+  class RightClickListener extends MouseAdapter {
+
+    public void Pop(MouseEvent e) {
+      showList list = new showList();
+      list.show(e.getComponent(), e.getX(), e.getY());
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+      if(e.isPopupTrigger())
+        Pop(e);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+      if(e.isPopupTrigger())
+        Pop(e);
+    }
+  }
+
+  public void propertiesClick() {
+
   }
   
-  public ChangingProperties(){
-    super("Xcos on Desktop");
-    setSize(300, 400);
-    setResizable(true);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
-    add(label);
-    setVisible(true);
+  public ChangingProperties() {
+    frame = new JFrame();
+    label = new JLabel("Changing Properties");
+    panel = new JPanel();
+    frame.setTitle("Part - A(1)");
+    frame.setSize(300, 400);
+    frame.setResizable(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    panel.setLayout(new GridBagLayout());
+    label.addMouseListener(new RightClickListener());
+    panel.add(label);
+    frame.add(panel);
+    frame.setVisible(true);
+  }
+
+  public static void main(String[] args){
+    new ChangingProperties();
   }
 }
 
