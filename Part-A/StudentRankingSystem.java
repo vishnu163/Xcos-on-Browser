c66b7b52cc3829026a527e837072de68990d37af
Vishnu <vishnuhnair1996@gmail.com>
2016-04-08 15:22:47 +0530

    Only commenting left

--- Part-A/StudentRankingSystem.java
+++ Part-A/StudentRankingSystem.java
@@ -1,12 +1,19 @@
+/*
+	Name: Vishnu H Nair
+	
+*/
 import java.util.Scanner;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Arrays.*;
 
-public class StudentRankingSystem {
+class StudentRankingSystem implements Comparable<StudentRankingSystem>{
 
 	Scanner reader = new Scanner(System.in);
 	private int rollNo;
 	private String name;
 	private float lang1, lang2, math, science, es;
-	private float total = 0;
+	private float total;
 	
 	public StudentRankingSystem() {
 		
@@ -30,63 +37,118 @@ public class StudentRankingSystem {
 		
 		System.out.print("Marks in Environmental Studies: ");
 		es = reader.nextFloat();
+
+		System.out.println("");
+
+		total = lang1 + lang2 + math + science + es;
 		
 	}
 	
 	public String getName() {
+
 		return name;
 	}
 	
 	public int getRoll() {
+
 		return rollNo;
 	}
 	
 	public float getLang1() {
+
 		return lang1;
 	}
 	
 	public float getLang2() {
+
 		return lang2;
 	}
 	
 	public float getMath() {
+
 		return math;
 	}
 	
 	public float getScience() {
+
 		return science;
 	}
 	
 	public float getES() {
+
 		return es;
 	}
 	
 	public float getTotal() {
-		total = lang1 + lang2 + math + science + es;
+
 		return total;
 	}
 	
-	public void PrintRollNo() {
-		System.out.println("Roll No :" + rollNo);
-	}
-	
-	public void Rank(StudentRankingSystem s[]) {
-		
+	public int compareTo(StudentRankingSystem student) {
+
+		if(this.total > student.getTotal())
+			return 1;
+		if(this.total < student.getTotal())
+			return -1;
+
+		if(this.math > student.getMath())
+			return 1;
+		if(this.math < student.getMath())
+			return -1;
+
+		if(this.science > student.getScience())
+			return 1;
+		if(this.science < student.getScience())
+			return -1;
+
+		if(this.es > student.getES())
+			return 1;
+		if(this.es < student.getES())
+			return -1;
+
+		if(this.lang1 > student.getLang1())
+			return 1;
+		if(this.lang1 < student.getLang1())
+			return -1;
+
+		if(this.lang2 > student.getLang2())
+			return 1;
+		if(this.lang2 < student.getLang2())
+			return -1;
+
+		return -1*this.name.compareTo(student.name);
 	}
 	
 	
 	public static void main(String[] args) {
+
 		Scanner reader = new Scanner(System.in);
 		int n;
+		System.out.println("\n************************ Student Ranking System ************************\n");
 		System.out.print("Enter the number of students: ");
 		n = reader.nextInt();
+		while(n<5) {
+			System.out.println("\nNumber of students must be atleast 5. Try again! \n");
+			System.out.print("Enter the number of students: ");
+			n = reader.nextInt();
+		}
 
 		StudentRankingSystem[] student = new StudentRankingSystem[n];
 		for(int i = 0; i < n; i++) {
 			System.out.printf("Enter the following details of Student %d \n" , i+1);
 			student[i] = new StudentRankingSystem();
 		}
-		
+
+		Arrays.sort(student,0,n);
+
+		System.out.println("Rank\t\tRoll No\t\tName\t\tTotal\t\tMathes\t\tScience\t\tES\t\tLang 1\t\tLang 2");
+
+		for(int i = n-1; i >= 0; i--) {
+	 		System.out.println((n-i) + "\t\t" + student[i].rollNo + "\t\t" + student[i].name + "\t\t" + student[i].total + "\t\t" +
+	 						   student[i].math + "\t\t" + student[i].science + "\t\t" + student[i].es + "\t\t" + student[i].lang1 +
+	 						   "\t\t" + student[i].lang2); 		
+	 	}
+
 		reader.close();
 	}
 	
